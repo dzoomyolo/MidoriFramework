e6e1f2392377ed41d965dda48945f4a04fb8fbfd
dzoom <dzoomfeed142@gmail.com>
2020-05-07 15:09:53 +0300

    add function log

--- lib/classes/LoggerClass.php
+++ lib/classes/LoggerClass.php
@@ -4,21 +4,33 @@ namespace lib\classes;
 
 class LoggerClass{
     
+    private $PATH_FOLDER;
     private $PATH;
+    private $FILE;
 
     function __construct($fileFolder){
-        $this->$PATH = $fileFolder;
+        $this->PATH_FOLDER = $fileFolder;
+        $this->PATH = $this->PATH_FOLDER.date("Y-m-d")."-log.txt"; 
         $this->init();
     }
-    
     private function init(){
-        d($this->$PATH."log.txt");
-        $file = fopen($this->$PATH."log.txt", "w+");
-        fwrite($file, "------ MidoriFramework log file ------");
-        fclose($file);
+        if(!file_exists($this->PATH)){
+            $this->FILE = fopen($this->PATH, 'a');
+            fwrite($this->FILE, "------ MidoriFramework log file ------\n");
+        }else{
+            $this->FILE = fopen($this->PATH, 'a');
+        }
     }
-
-
+    private function __write($s){
+        file_put_contents($this->PATH, $s, FILE_APPEND | LOCK_EX);
+    }
+    public function log($string){
+        $s = '['.date('D M d H:i:s Y',time()).'] '.$string."\n";
+        $this->__write($s);
+    }
+    function __destruct(){
+        fclose($this->FILE);
+    }
 }
 
 ?>
