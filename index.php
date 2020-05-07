4388b7cd67fe5f50e18b6b75fe8064f01a2db7c7
dzoom <dzoomfeed142@gmail.com>
2020-05-07 13:36:39 +0300

    add new classes

--- index.php
+++ index.php
@@ -4,10 +4,18 @@ require './lib/declarations/main.php';
 
 use lib\classes\DatabaseClass;
 
+use lib\classes\URLClass;
+
 use lib\classes\RouterClass;
+
+use lib\classes\LoggerClass;
 
 $db = new DatabaseClass(array("dsn"=>"mysql:host=localhost;dbname=anim","user"=>"root","pass"=>""));
 
+$url = new URLClass();
+
 $router = new RouterClass();
+
+$logger = new LoggerClass();
 
 ?>
